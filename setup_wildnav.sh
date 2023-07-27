7b5ee397dc4d649014b93687e7d5d696851997f6
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2023-07-27 12:29:05 -0400

    Setup for a local environment.

---
+++ setup_wildnav.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create and activate venv.
+python3 -m venv env 
+source env/bin/activate
+
+# Set up submodule and requirements.
+git submodule update --init --recursive
+pip3 install -r requirements.txt
