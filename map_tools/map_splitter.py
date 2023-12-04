6f7220d3479c6c01eef0668a2dee06d7ddb41215
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2023-12-04 14:16:48 -0500

    Fixed default param and docker setup

--- map_tools/map_splitter.py
+++ map_tools/map_splitter.py
@@ -14,7 +14,7 @@ def main():
     args = parser.parse_args()
 
     image_path = args.IMAGE_PATH
-    size = int(args.size)
+    size = int(args.size) if args.size else None
     out_format = args.of
 
     out_images = map.split_map(image_path, size, out_format)
